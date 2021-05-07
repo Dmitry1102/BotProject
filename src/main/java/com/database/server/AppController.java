d4e37d4eb385f5403d03b435506e95c99176f830
Dmitry1102 <meleshkevich2016@mail.ru>
2021-05-07 15:08:16 +0200

    Branch1 add6

--- src/main/java/com/database/server/AppController.java
+++ src/main/java/com/database/server/AppController.java
@@ -3,7 +3,11 @@ package com.database.server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -26,6 +30,20 @@ public class AppController {
         Configurations config = new Configurations();
         model.addAttribute("city", config);
         return "new_city";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String saveProduct(@ModelAttribute("configurations") Configurations configurations) {
+        service.save(configurations);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/edit/{id}")
+    public ModelAndView showEditProductPage(@PathVariable(name = "id") int id) {
+        ModelAndView mav = new ModelAndView("edit_product");
+        Configurations configurations = service.get(id);
+        mav.addObject("configurations", configurations);
+        return mav;
     }
 
 }
