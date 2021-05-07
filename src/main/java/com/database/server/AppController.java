72d824f0971b70029520b156b7f2fb219c27be83
Dmitry1102 <meleshkevich2016@mail.ru>
2021-05-07 14:54:23 +0200

    Branch1 add5

--- src/main/java/com/database/server/AppController.java
+++ src/main/java/com/database/server/AppController.java
@@ -2,6 +2,10 @@ package com.database.server;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class AppController {
@@ -10,5 +14,18 @@ public class AppController {
     private CityService service;
 
 
+    @RequestMapping("/")
+    public String viewHomePage(Model model) {
+        List<Configurations> listProducts = service.listAll();
+        model.addAttribute("listProducts", listProducts);
+        return "index";
+    }
+
+    @RequestMapping("/new")
+    public String showNewProductPage(Model model) {
+        Configurations config = new Configurations();
+        model.addAttribute("city", config);
+        return "new_city";
+    }
 
 }
--- src/main/java/com/telegram/project/App.java
+++ src/main/java/com/telegram/project/App.java
@@ -10,10 +10,10 @@ public class App {
     private static final Logger log = Logger.getLogger(App.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
         ApiContextInitializer.init();
         Bot testBot = new Bot("TestTMS_bot", "1773180686:AAF890sGnfjEil_6ualF8dslmSywbeu7HtI");
         testBot.botConnect();
+        SpringApplication.run(App.class, args);
     }
 }
 
